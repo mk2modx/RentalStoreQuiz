0a155791accb889890bdb96767fc274275070198
Mark Ag <mk2modx@icloud.com>
2019-08-22 15:10:35 -0600

    Quiz

--- src/com/skilldistillery/application/RentalStoreQuizApp.java
+++ src/com/skilldistillery/application/RentalStoreQuizApp.java
@@ -8,54 +8,63 @@ import com.skilldistillery.entities.Store;
 
 public class RentalStoreQuizApp {
 
+
+	Store st = new Store();
 	public static void main(String[] args) {
 		RentalStoreQuizApp rents = new RentalStoreQuizApp();
 
-		// We are creating a film rental store application. 
+		// We are creating a film rental store application.
 		// The store can have any number of unique customers.
-		
+
 		// create a store instance
 		Store s = rents.createStore();
 
 		// Fred Flintstone is excited to join the store as a Customer.
 		// Create Fred Flintstone as a customer
 		Customer ct = rents.createCustomer("Fred", "Flintstone");
-
-		// Fred rents the movie: "Booleans: even if you are wrong, you are only off by a bit"
-		//  ... add the film to Fred's collection of rentals
+		// Fred rents the movie: "Booleans: even if you are wrong, you are only off by a
+		// bit"
+		// ... add the film to Fred's collection of rentals
 		ct.addRental(new Film("Booleans: even if wrong, you're only off by a bit"));
-		
+
 		// ... and register Fred Flintstone as a member of the store's unique customers
 		rents.registerCustomerAtStore(s, ct);
-		
+
+		System.out.println(s);
+		System.out.println(ct);
 		// Elmer Fudd is excited to join the store as a Customer.
 		// Create Elmer Fudd as a customer
 		Customer ct2 = rents.createCustomer("Elmer", "Fudd");
 
 		// ... and register Elmer Fudd as a member of the store's unique customers
 		rents.registerCustomerAtStore(s, ct2);
-		
+
 		// display all customer information at this store:
 		rents.displayData(s);
 	}
 
 	public Store createStore() {
 		// TODO: implement the method
-		return null;
+		st = new Store();
+
+		return st;
 	}
 
 	public Customer createCustomer(String fn, String ln) {
 		// TODO: implement the method
-		return null;
+		Customer newCustomer = new Customer(fn, ln);
+
+		return newCustomer;
 	}
 
 	public void registerCustomerAtStore(Store st, Customer ct) {
 		// TODO: implement the method
-		
+		st.addCustomer(ct);
+
 	}
-	
-	public void displayData (Store s) {
-		// TODO: implement the method
-	
+
+	public void displayData(Store s) {
+		st.toString();
+
 	}
 }
--- src/com/skilldistillery/entities/Store.java
+++ src/com/skilldistillery/entities/Store.java
@@ -1,15 +1,40 @@
 package com.skilldistillery.entities;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class Store {
+
+
+public class Store extends Customer{
 	// TODO: each store has an id
+	private int id;
 	
 	// TODO: each store has 0 or more unique Customers
+	 List <Customer> customerList;
 	
 	// TODO: implement the required methods to:
 	//       * Add a customer (no duplicates allowed)
 	//       * Return the number of customers
 	//       * Display all data
+	
+	public List <Customer> addCustomer(Customer m) {
+		List<Customer> customerList = new ArrayList<>();
+		 customerList.add(m);
+		 
+		return customerList;
+	}
+	
+	public int returnCustomerSize () {
+		return customerList.size();
+	}
+
+	@Override
+	public String toString() {
+		return "Store [id=" + id + ", customer=" + customerList + "]";
+	}
+	
+	
+	
 }
